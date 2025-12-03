08679c5c3edc849f065cc51cc47a2532dfe9ee5d
DevAiKaiSys <dev.aikaisys@gmail.com>
2025-12-03 14:12:20 +0700

    Use @for to list objects in component

--- src/app/housing-location/housing-location.spec.ts
+++ src/app/housing-location/housing-location.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HousingLocation } from './housing-location';
+import { HousingLocationInfo } from '../housinglocation';
 
 describe('HousingLocation', () => {
   let component: HousingLocation;
@@ -10,10 +11,25 @@ describe('HousingLocation', () => {
     await TestBed.configureTestingModule({
       imports: [HousingLocation]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(HousingLocation);
     component = fixture.componentInstance;
+
+    const mockHousingLocation: HousingLocationInfo = {
+      id: 999,
+      name: 'Test Home',
+      city: 'Test City',
+      state: 'ST',
+      photo: '/assets/example.jpg',
+      availableUnits: 1,
+      wifi: true,
+      laundry: false
+    };
+
+    fixture.componentRef.setInput('housingLocation', mockHousingLocation);
+    fixture.detectChanges();
+
     await fixture.whenStable();
   });
 
