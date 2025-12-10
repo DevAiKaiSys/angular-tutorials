654b1000cdeaa731168dc1c913c620616cdd18af
DevAiKaiSys <dev.aikaisys@gmail.com>
2025-12-10 12:06:12 +0700

    Event handling

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,17 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "app-root",
-  styleUrls: ['app.css'],
   template: `
-    <div [contentEditable]="isEditable"></div>
+    <section (mouseover)="showSecretMessage()">
+      There's a secret message for you, hover to reveal:
+      {{ message }}
+    </section>
   `,
 })
 export class App {
-  isEditable = true;
+  message = '';
+
+  showSecretMessage() {
+    this.message = 'Way to go 🚀';
+  }
 }
