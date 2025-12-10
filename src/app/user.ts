3a8df05fde08923ca93dea619941770055a49d11
DevAiKaiSys <dev.aikaisys@gmail.com>
2025-12-10 14:01:59 +0700

    Optimizing images

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,11 +1,26 @@
-import { Component, input } from "@angular/core";
+import { NgOptimizedImage } from "@angular/common";
+import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-user',
     template: `
-    <p>The user's name is {{ name() }}</p>
+    <p>Username: {{ username }}</p>
+    <p>Preferred Framework:</p>
+    <ul>
+      <li>
+        Static Image:
+        <img ngSrc="/assets/logo.svg" alt="Angular logo" width="32" height="32" />
+      </li>
+      <li>
+        Dynamic Image:
+        <img [ngSrc]="logoUrl" [alt]="logoAlt" width="32" height="32" />
+      </li>
+    </ul>
   `,
+    imports: [NgOptimizedImage],
 })
 export class User {
-    readonly name = input<string>();
+    logoUrl = '/assets/logo.svg';
+    logoAlt = 'Angular logo';
+    username = 'youngTech';
 }
