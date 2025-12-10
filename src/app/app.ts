81ffb520f7e35ff7f6569e9410bb7854782fa040
DevAiKaiSys <dev.aikaisys@gmail.com>
2025-12-10 11:25:48 +0700

    Updating the Component Class

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,13 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "app-root",
-  template: `Hello Universe`,
+  template: `Hello {{ city }}, {{ 1 + 1 }}`,
   styles: `
     :host {
       color: #a144eb;
     }
   `,
 })
-export class App {}
+export class App {
+  city = 'San Francisco';
+}
